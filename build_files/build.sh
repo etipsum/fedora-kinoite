801c3ca1dfdc0ce1c7aee3e8874565b85243f4f1
Bekhruz Sharipov <dev.bekhruz.sharipov@gmail.com>
2025-07-12 00:17:00 +0300

    Add alacritty, rclone and create directory for Nix

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux
+dnf5 install -y alacritty
+dnf5 install -y rclone
 
 # Use a COPR Example:
 #
@@ -21,4 +23,7 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+
+# Create a directory for Nix
+mkdir /nix
